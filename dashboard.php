291617b1370d1cf270894beacacaa06cede8f36f
jdaniel0607 <jhonda429@gmail.com>
2024-12-07 17:55:40 -0500

    se modifica dashboard, se agrega header y footer, se aplica bootstrap enestilo en div contenedor de la informacion actual del usuario

--- dashboard.php
+++ dashboard.php
@@ -72,22 +72,63 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['actualizar'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="script.js"></script>
 </head>
+<!--comienzo header-->
+<header class="container d-inline p-1 justify-content-center">
+  <nav class="navbar navbar-expand-lg w-100">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">
+        <img src="images/logo-emprendedores.png">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse align-items-end flex-column" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Iniciar sesión</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="registro.php">Registrarse</a>
+          </li>
+          <li class="nav-item button-header p-0 m-0">
+            <a class="nav-link a-button-header" aria-current="page" href="consulta.php">Consultar emprendedores</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
+<!--fin del header-->
+
+<!--inicio body-->
+
+
 <body class="container mt-5">
     <h1>Bienvenido, <?php echo $usuario['nombre'] . ' ' . $usuario['apellido']; ?>!</h1>
     
-    <div class="card p-4">
-        <h3>Detalles del Usuario</h3>
+    <div class="mb-4">
+        <h2 class="mb-1">Detalles del usuario</h2>
+        <hr class="dashed">
         <p><strong>Correo:</strong> <?php echo $usuario['email']; ?></p>
+        <hr class="dashed">
         <p><strong>Documento:</strong> <?php echo $usuario['documento']; ?></p>
+        <hr class="dashed">
         <p><strong>Teléfono:</strong> <?php echo $usuario['telefono']; ?></p>
+        <hr class="dashed">
         <p><strong>Nombre de la Empresa:</strong> <?php echo $usuario['nombre_empresa']; ?></p>
+        <hr class="dashed">
         <p><strong>Ciudad de la Empresa:</strong> <?php echo $usuario['ciudad']; ?></p>
+        <hr class="dashed">
         <p><strong>Departamento:</strong> <?php echo $usuario['departamento']; ?></p>
-       
+        <hr class="dashed">
         <p><strong>Descripción de la Empresa:</strong> <?php echo $usuario['descripcion']; ?></p>
+        <hr class="dashed">
     </div>
 
-    <h3>Actualizar Datos</h3>
+<!-- de aqui para abajo se cambió-->
+
+<h3>Actualizar Datos</h3>
     <form id="formActualizarDatos" method="POST">
         <div class="form-group">
             <label for="nombre">Nombre:</label>
@@ -122,6 +163,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['actualizar'])) {
             <label for="ciudad">Ciudad:</label>
             <input type="text" name="ciudad" id="ciudad" class="form-control" value="<?php echo $usuario['ciudad']; ?>">
         </div>
+
+
+
+<!-- de aqui para abajo original-->
+
+
         <button type="submit" name="actualizar" class="btn btn-primary" id="btnActualizar">Actualizar Datos</button>
     </form>
 
@@ -146,4 +193,37 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['actualizar'])) {
     ?>
 
 </body>
+<!--fin body-->
+
+<!--inicio footer-->
+<footer class="bd-footer py-2 py-md-2 mt-5 bg-body-tertiary">
+  <div class="container py-2 py-md-5 px-2 px-md-3 text-body-secondary">
+    <div class="row d-flex justify-content-around">
+      <div class="col-lg-4 col-mb-1 mb-3 d-flex flex-column align-items-center">
+        <a>
+          <img src="images/logo-emprendedores.png">
+        </a>
+      </div>
+      <div class="col-6 col-lg-2 mb-3">
+        <ul class="list-unstyled">
+          <li class="mb-2"><a href="login.php">Inciar sesión</a></li>
+          <li class="mb-2"><a href="registro.php">Registro</a></li>
+          <li class="mb-2"><a href="terminos-condiciones.php">Términos y condiciones</a></li>
+        </ul>
+      </div>
+      <div class="col-12 col-lg-4 mb-3 d-flex flex-column align-items-center">
+        <a class="col " href="consulta.php">
+          <button class="rounded-2">
+            Consultar emprendedores
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="d-flex align-content-center justify-content-center">
+    <p>Creat by Emprendeduros Talento-tech-2024</p>
+  </div>
+</footer>
+<!--fin footer-->
+
 </html>
